43f6cd244e9cdca6d2a4d296666da2af5aaec7b1
TE_Raghavendra_GP <raghavendra.g@testyantra.in>
2023-09-27 15:50:31 +0530

    updated responsiveness in subcontent

--- src/components/SubContent.jsx
+++ src/components/SubContent.jsx
@@ -1,54 +1,60 @@
 import React, { Fragment } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import SubContentCss from "./MainContentCss.module.css";
 
 const SubContent = () => {
   return (
     <Fragment>
-      <div style={{ paddingTop: "5%", paddingBottom: "5%" }}>
-        <div style={{ padding: "3rem 1.5rem" }}>
-          <iframe
-            width="800px"
-            height="400px"
-            src="https://www.youtube.com/embed/J3usOwPYRbA?si=CK3bh4fpUhbXfAz5"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-            style={{ borderRadius: "10px" }}
-          ></iframe>
-        </div>
-
-        <div className="container">
-          <div className="row" style={{ paddingTop: "5%" }}>
-            <div
-              className={`${SubContentCss.flag_video}col d-flex col-lg-6 col-sm-12-items-center`}
+      <div style={{ paddingTop: "5%" }}>
+        <Container fluid className="py-5">
+          <Row className="justify-content-center">
+            <Col sm={12} md={8} lg={6}>
+              <iframe
+                width="100%"
+                height="400px"
+                src="https://www.youtube.com/embed/J3usOwPYRbA?si=CK3bh4fpUhbXfAz5"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                className={SubContentCss.videoIframe}
+                style={{ borderRadius: "10px" }}
+              ></iframe>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row className="py-5">
+            <Col
+              sm={12}
+              lg={6}
+              className={`d-flex align-items-center ${SubContentCss.flagVideo}`}
             >
               <iframe
-                width="560"
+                width="100%"
                 height="315"
                 src="https://www.youtube.com/embed/ADfivEQV24U?si=Kcodr1UpePGRzNTw"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
+                allowFullScreen
+                className={SubContentCss.videoIframe}
                 style={{ borderRadius: "10px" }}
               ></iframe>
-            </div>
-            <div className="text-center col-lg-5 col-sm-12 offset-lg-1">
-              <div className="row">
-                <h3 className={`${SubContentCss.vid_title} my-4`}>
+            </Col>
+            <Col sm={12} lg={5} className="offset-lg-1">
+              <div className="text-center">
+                <h3 className={`${SubContentCss.vidTitle} my-4`}>
                   Har Ghar Tiranga
                 </h3>
-                <p className={SubContentCss.video_text}>
+                <p className={SubContentCss.videoText}>
                   The Tri-colour flag is a symbol of pride for every Indian. It
                   represents national integrity and signifies the hopes and
                   aspirations of the Indian people.
                 </p>
               </div>
-            </div>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </Fragment>
   );
